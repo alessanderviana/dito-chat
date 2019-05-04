a7edc67a881b5a17d986f5fa2649d3019455729f
alessanderviana <alessander.viana@hotmail.com>
2019-05-04 09:34:53 -0300

    Removed the section used in the user env preparation file

--- kubernetes/01-kube-cluster-preparing.sh
+++ kubernetes/01-kube-cluster-preparing.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-# kubeadm init
-
-# Configure the current user
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
 # Create the wave-net pod
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
@@ -14,6 +7,6 @@ kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl versio
 echo "source <(kubectl completion bash)" >> .bashrc
 
 # Print Master token
-echo -e "\n####################################################"
+echo -e "\n########################################################################################################"
 kubeadm token create --print-join-command
-echo -e "####################################################\n\n"
+echo -e "########################################################################################################\n\n"
