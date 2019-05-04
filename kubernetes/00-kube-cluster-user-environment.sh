3d458d0f7a4bcf0bfad904ab69fb3b84ed26b63a
alessanderviana <alessander.viana@hotmail.com>
2019-05-04 17:21:30 -0300

    Changed the cluster preparing file

--- kubernetes/00-kube-cluster-user-environment.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Configure the current user
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
--- kubernetes/01-kube-cluster-preparing.sh
+++ kubernetes/01-kube-cluster-preparing.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# Cluster init
+# kubeadm init
+
+# Configure the current user
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
 # Create the wave-net pod
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
